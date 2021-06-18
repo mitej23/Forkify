88e5b29fe9213acbbcd8d0f75460df65a9360b66
mitej23 <mitejmadan@gmail.com>
2021-06-19 00:26:40 +0530

    created model for search query

--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { API_URL } from "./config";
 import { getJSON } from "./helpers";
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (hash) {
   try {
-    const data = await getJSON(`${API_URL}/${hash}`);
+    const data = await getJSON(`${API_URL}${hash}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -21,6 +25,26 @@ export const loadRecipe = async function (hash) {
       ingredients: recipe.ingredients,
     };
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+    state.search.results = data.data.recipes.map((rec) => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.log(err);
+    throw error;
+  }
+};
+
