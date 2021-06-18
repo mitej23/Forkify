d990f0a8f6f542efb040b13227dc8218aa922624
mitej23 <mitejmadan@gmail.com>
2021-06-19 00:25:05 +0530

    adde error,message and event handler

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,9 @@ import { Fraction } from "fractional";
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
   #data;
+  #errorMessage = "We could not find that recipe. Pease try another one!";
+  #message = " ";
+
   render(data) {
     this.#data = data;
     const markup = this.#generateMarkup();
@@ -14,7 +17,7 @@ class RecipeView {
   #clear() {
     this.#parentElement.innerHTML = "";
   }
-  renderSpinner = function () {
+  renderSpinner() {
     const markup = `
     <div class="spinner">
       <svg>
@@ -22,20 +25,57 @@ class RecipeView {
       </svg>
     </div>
     `;
-    this.#parentElement.innerHTML = "";
+    this.#clear();
     this.#parentElement.insertAdjacentHTML("afterbegin", markup);
-  };
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+    <div class="error">
+      <div>
+        <svg>
+          <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+    <div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>
+    `;
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  addHandlerRender(handler) {
+    ["hashchange", "load"].forEach((ev) =>
+      window.addEventListener(ev, handler)
+    );
+  }
 
   #generateMarkup() {
+    console.log(this.#data);
     return `
     <figure class="recipe__fig">
-    <img src="${this.#data.image_url}" alt="${
+      <img src="${this.#data.image_url}" alt="${
       this.#data.title
     }" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${this.#data.title}</span>
-    </h1>
-</figure>
+      <h1 class="recipe__title">
+        <span>${this.#data.title}</span>
+      </h1>
+    </figure>
 
   <div class="recipe__details">
     <div class="recipe__info">
@@ -43,7 +83,7 @@ class RecipeView {
         <use href="${icons}#icon-clock"></use>
       </svg>
       <span class="recipe__info-data recipe__info-data--minutes">${
-        this.#data.cooking_time
+        this.#data.cookingTime
       }</span>
       <span class="recipe__info-text">minutes</span>
     </div>
@@ -100,7 +140,7 @@ class RecipeView {
     </p>
     <a
       class="btn--small recipe__btn"
-      href="${this.#data.source_url}"
+      href="${this.#data.sourceUrl}"
       target="_blank"
     >
       <span>Directions</span>
