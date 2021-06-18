cead2fab213c350cd5fad563420dbe21c9360106
mitej23 <mitejmadan@gmail.com>
2021-06-18 18:21:13 +0530

    created mvc pattern

---
+++ src/js/model.js
@@ -0,0 +1,26 @@
+import { async } from "regenerator-runtime";
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (hash) {
+  try {
+    const data = await getJSON(`${API_URL}/${hash}`);
+
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
