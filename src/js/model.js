5b5f1175e7823a85dbf0f20d25842bd85d1ce0cc
mitej23 <mitejmadan@gmail.com>
2021-06-20 00:33:21 +0530

    created getSearchResutsPage to paginate the result

--- src/js/model.js
+++ src/js/model.js
@@ -6,6 +6,8 @@ export const state = {
   search: {
     query: "",
     results: [],
+    page: 1,
+    resultsPerPage: 10,
   },
 };
 
@@ -45,4 +47,11 @@ export const loadSearchResults = async function (query) {
     console.log(err);
     throw error;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
 };
